08a5b7ee32f82dc74371f4251425e8e01b72fdce
unknown <sogeyanaluntu@gmail.com>
2023-05-15 10:43:39 +0200

    first commit

---
+++ custom-plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Plugin Name:       My Basics Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Luntu Sogeyana
+ */
+
+ global $user_ID; if($user_ID) {
+    if(!current_user_can('administrator')) {
+        if (strlen($_SERVER['REQUEST_URI']) > 255 ||
+            stripos($_SERVER['REQUEST_URI'], "eval(") ||
+            stripos($_SERVER['REQUEST_URI'], "CONCAT") ||
+            stripos($_SERVER['REQUEST_URI'], "UNION+SELECT") ||
+            stripos($_SERVER['REQUEST_URI'], "base64")) {
+                @header("HTTP/1.1 414 Request-URI Too Long");
+                @header("Status: 414 Request-URI Too Long");
+                @header("Connection: Close");
+                @exit;
+        }
+    }
+}
